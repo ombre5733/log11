b297596c82ca034dd207616987733e73e4c0b314
Manuel Freiberger <manuel.freiberger@gmx.at>
2016-03-04 22:25:29 +0100

    Added a ring buffer.
    Added a ring buffer based on the disruptor pattern with some simplifications as there will only
    be one consumer.

---
+++ src/ringbuffer.cpp
@@ -0,0 +1,100 @@
+/*******************************************************************************
+  Copyright (c) 2016, Manuel Freiberger
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#include "ringbuffer.hpp"
+
+#include <chrono>
+#include <thread>
+
+
+RingBuffer::RingBuffer(unsigned elementSize, unsigned size)
+    : m_elementSize(elementSize),
+      m_totalNumElements((size + elementSize - 1) / elementSize),
+      m_claimed(0),
+      m_published(0),
+      m_consumed(0)
+{
+    m_data = ::operator new(m_elementSize * m_totalNumElements);
+}
+
+RingBuffer::~RingBuffer()
+{
+    ::operator delete(m_data);
+}
+
+auto RingBuffer::claim(unsigned numElements) -> Range
+{
+    // TODO: limit numElements to m_size
+
+    // Claim a sequence of elements.
+    unsigned claimEnd = m_claimed += numElements;
+    // Check if the claimed elements have been consumed already.
+    unsigned consumerThreshold = claimEnd - m_totalNumElements;
+    while (int(consumerThreshold - m_consumed) > 0)
+    {
+        // Wait until the consumer has made progress.
+        // TODO: Implement a better strategy.
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+
+    return Range(claimEnd - numElements, numElements);
+}
+
+auto RingBuffer::tryClaim(unsigned numElements, bool allowTruncation) -> Range
+{
+    // TODO: limit numElements to m_size
+
+    unsigned claimBegin = m_claimed;
+    int free;
+    do
+    {
+        // Determine the number of available elements.
+        free = m_consumed - (claimBegin - m_totalNumElements);
+        if (free >= int(numElements))
+            free = numElements;
+        else if (free <= 0 || !allowTruncation)
+            return Range(0, 0);
+    } while (!m_claimed.compare_exchange_weak(claimBegin, claimBegin + free));
+
+    return Range(claimBegin, free);
+}
+
+void RingBuffer::publish(const Range& range)
+{
+    if (m_published != range.begin)
+    {
+        // Wait until the other producers have made progress.
+        // TODO: Implement a better strategy.
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+
+    m_published = range.begin + range.length;
+}
+
+void* RingBuffer::operator[](unsigned index)
+{
+    return static_cast<char*>(m_data) + (index % m_totalNumElements) * m_elementSize;
+}
---
+++ src/ringbuffer.hpp
@@ -0,0 +1,91 @@
+/*******************************************************************************
+  Copyright (c) 2016, Manuel Freiberger
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#ifndef LOG11_RINGBUFFER_HPP
+#define LOG11_RINGBUFFER_HPP
+
+#include <atomic>
+
+class RingBuffer
+{
+public:
+    struct Range
+    {
+        Range(unsigned b, unsigned l)
+            : begin(b), length(l)
+        {
+        }
+
+        unsigned begin;
+        unsigned length;
+    };
+
+    //! Creates a ring buffer whose slots are of size \p elementSize. The
+    //! total amount of memory occupied by the ring buffer is \p size (rounded
+    //! up to the next multiple of \p elementSize).
+    RingBuffer(unsigned elementSize, unsigned size);
+
+    //! Destroys the ring buffer.
+    ~RingBuffer();
+
+    RingBuffer(const RingBuffer&) = delete;
+    RingBuffer& operator=(const RingBuffer&) = delete;
+
+    //! Claims \p numElements slots from the ring buffer. The caller is
+    //! blocked until the slots are free.
+    Range claim(unsigned numElements);
+
+    //! Tries to claim \p numElements slots from the ring buffer without
+    //! blocking the caller. If the buffer is full, an empty range is
+    //! returned. When the number of available elements is less than
+    //! \p numElements, the behaviour depends on \p allowTruncation: If
+    //! the flag is set, a range smaller than \p numElements may be returned
+    //! otherwise, an empty range is returned.
+    Range tryClaim(unsigned numElements, bool allowTruncation);
+
+    //! Publishes the slots specified by the \p range. The range must have
+    //! been claimed before publishing. The caller will be blocked until
+    //! all prior producers have published their slots.
+    void publish(const Range& range);
+
+    //! Returns a pointer to the \p index-th slot.
+    void* operator[](unsigned index);
+
+private:
+    void* m_data;
+    //! The size of one slot in the buffer.
+    unsigned m_elementSize;
+    unsigned m_totalNumElements;
+
+    //! Points past the last claimed slot.
+    std::atomic<unsigned> m_claimed;
+    //! Points past the last published slot.
+    std::atomic<unsigned> m_published;
+    //! Points past the last consumed slot.
+    std::atomic<unsigned> m_consumed;
+};
+
+#endif // LOG11_RINGBUFFER_HPP

f2ae4852e130865c3d50340a6e66d0cf593ba015
Manuel Freiberger <manuel.freiberger@gmx.at>
2016-03-11 04:35:57 +0100

    Added a StringRef class.

---
+++ src/stringref.hpp
@@ -0,0 +1,62 @@
+/*******************************************************************************
+  Copyright (c) 2016, Manuel Freiberger
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#ifndef LOG11_STRINGREF_HPP
+#define LOG11_STRINGREF_HPP
+
+#include <cstddef>
+
+
+namespace log11
+{
+
+class StringRef
+{
+public:
+    StringRef(const char* str, std::size_t length)
+        : m_data(str),
+          m_length(length)
+    {
+    }
+
+    const char* data() const noexcept
+    {
+        return m_data;
+    }
+
+    std::size_t size() const noexcept
+    {
+        return m_length;
+    }
+
+private:
+    const char* m_data;
+    std::size_t m_length;
+};
+
+} // namespace log11
+
+#endif // LOG11_STRINGREF_HPP
